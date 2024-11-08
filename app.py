1789b8b916caa16a6118378dbeb8cd4ab1f9e007
Adebayo Olaonipekun <pekunmi@live.com>
2024-11-08 00:30:29 +0000

    Commented out fastapi

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import requests
+import joblib
+import numpy as np
+
+# Load model
+model = joblib.load("model/model.pkl")
 
 # app title and description
 st.title("Power Output Prediction")
@@ -13,20 +18,24 @@ relative_humidity = st.number_input("Relative Humidity (RH)", value=75.0)
 
 # Create a button for making the prediction
 if st.button("Predict"):
+    input_data = np.array([[ambient_temp, exhaust_vacuum, ambient_pressure, relative_humidity]])
+    prediction = model.predict(input_data)
+    st.write(f"Predicted Power Output (PE): {prediction[0]}")
+
+    # Use the codes below when connecting via fastapi
     # prepare data
-    input_data = {
-        "AT": ambient_temp,
-        "V": exhaust_vacuum,
-        "AP": ambient_pressure,
-        "RH": relative_humidity
-    }
+    # input_data = {
+    #    "AT": ambient_temp,
+    #    "V": exhaust_vacuum,
+    #    "AP": ambient_pressure,
+    #    "RH": relative_humidity
+    # }
 
     # make api call
-    response = requests.post("http://127.0.0.1:8000/predict", json=input_data)
+    # response = requests.post("http://127.0.0.1:8000/predict", json=input_data)
 
-    if response.status_code == 200:
-        prediction = response.json()["prediction"]
-        st.write(f"Predicted Power Output (PE): {prediction}")
-    else:
-        st.write("Error in prediction. Please try again.")
-
+    # if response.status_code == 200:
+    #    prediction = response.json()["prediction"]
+    #    st.write(f"Predicted Power Output (PE): {prediction}")
+    # else:
+    #    st.write("Error in prediction. Please try again.")
